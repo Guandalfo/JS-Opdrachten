99cd23c356b31b291dab55f9d213f34482667f40
Guandalfo <Sneakynova@outlook.com>
2020-10-07 15:09:51 +0200

    H01 upload

---
+++ H01/H01.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+let i;
+for(i=0;i<10;i++){
+    console.log("De waarde van i is "+i)
+}
+var mijnauto={
+    merk:"Ford",
+    type:"Mondeo",
+    aantalWielen:"4",
+    kleur:"Blauw"
+}
+console.log(mijnauto.kleur)
