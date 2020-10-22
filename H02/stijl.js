1512be0645a2abcf222deb3165f8badf85cb93e3
Guandalfo <Sneakynova@outlook.com>
2020-10-22 12:58:38 +0200

    H02 upload

---
+++ H02/stijl.js
@@ -0,0 +1,8 @@
+document.getElementById("bl1").style.backgroundColor = "#22FFFF";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+document.getElementById("bl2").style.margin = "10px 0px 0px 0px";
+document.getElementById("bl3").style.margin = "40px 0px 0px 0px";
+document.getElementById("bl4").style.margin = "5px 0px 0px 0px";
+
---
+++ H02/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("title").innerHTML = "Dit is de pagina titel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById('aap1').src="/JS-Opdrachten/img/tomb_raider_1.jpg";
+document.getElementById('aap2').src="/JS-Opdrachten/img/Tomb raider 2 wallpaper.jpg";
